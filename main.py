9de643c035d830b333a011618fe7d74ed4c3d0cc
Bogdan Bedrylo <bogdan.bedrylo@gmail.com>
2020-12-22 01:17:45 +0200

    lab_5

---
+++ main.py
@@ -0,0 +1,78 @@
+import sys
+    
+
+def input_data(haystack_file_name, needle_file_name, delimiter_file_name):
+    try:
+        with open(haystack_file_name, 'r') as file:
+            haystack = file.readline()
+
+        with open(needle_file_name, 'r') as file:
+            needle = file.readline()
+
+        with open(delimiter_file_name, 'r') as file:
+            delimiter = file.readline()
+        return haystack, needle, delimiter
+               
+    except (FileNotFoundError):
+        sys.exit(f'file "{haystack_file_name}" or "{needle_file_name}" or' + 
+                    + '"{delimiter_file_name}" not found')
+    except:
+        sys.exit(f'check data in "{haystack_file_name}" and "{needle_file_name}' +
+                    + 'and "{delimiter_file_name}"')
+
+
+def output_data(file_name, output):
+    with open(file_name,'w') as file:
+        file.write(str(output))
+
+
+def prefix(string):
+    matches_num_list = [0]*len(string)
+
+    for i in range(1, len(string)):
+        k = matches_num_list[i-1]
+        while True:
+            if string[i] == string[k]:
+               matches_num_list[i] = k + 1 
+               break;
+            elif k == 0:
+                break;
+            else:
+                k = matches_num_list[k-1]
+
+    return matches_num_list
+
+
+def delimiter_in_strings(haystack, needle, delimiter):
+    for character in haystack + needle:
+        if character == delimiter:
+            return True
+    return False
+
+
+def main(haystack, needle, delimiter):
+    result = -1
+
+    if delimiter_in_strings(haystack, needle, delimiter):
+        sys.exit('choose other delimiter, which is missing in the strings')
+
+    combined_string = needle + delimiter + haystack
+    matches_num_list = prefix(combined_string)
+
+    idx = 0
+    for num in matches_num_list:
+        if num == len(needle):
+            return idx - 2 *len(needle)
+        idx += 1
+
+    return result
+
+
+if __name__=="__main__":
+
+    haystack, needle, delimiter = input_data('io/haystack.in', 'io/needle.in', 'io/delimiter.in');
+    result = main(haystack, needle, delimiter)
+    output_data('io/result.out', result)
+    print(result)
+
+
---
+++ test.py
@@ -0,0 +1,38 @@
+import unittest
+from main import *
+
+
+class Test(unittest.TestCase):
+
+	def test_input_data(self):
+		haystack, needle, delimiter = input_data('io/test/test_haystack.in',
+		 				'io/test/test_needle.in', 'io/test/test_delimiter.in')
+		self.assertEqual('gsagsagsagsagsagsagsagoog', haystack)
+		self.assertEqual('gsago', needle)
+		self.assertEqual('@', delimiter)
+
+	def test_output_data(self):
+		result = 15
+		file_path = 'io/test/test.out'
+		output_data(file_path, result)
+		with open(file_path, 'r') as file:
+			assert_result = file.readline()
+		self.assertEqual(assert_result, str(result))
+
+	def test_prefix(self):
+		result = prefix('aabaabaaaabaabaaabcca')
+		self.assertEqual(result, [0,1,0,1,2,3,4,5,2,2,3,4,5,6,7,8,9,3,0,0,1])
+
+	def test_delimiter_in_strings(self):
+		result = delimiter_in_strings('abbaabaaaabaabaaab', 'aabaa', 'a')
+		self.assertEqual(result, True)
+		result = delimiter_in_strings('abbaabaaaabaabaaab', 'aabaa', '@')
+		self.assertEqual(result, False)
+
+	def test_main(self):
+		result = main('abbaabaaaabaabaaab', 'aabaa', '@')
+		self.assertEqual(result, 3)
+
+
+if __name__ == "__main__":
+	unittest.main()
